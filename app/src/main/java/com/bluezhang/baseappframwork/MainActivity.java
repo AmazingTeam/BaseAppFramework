bd8229beb89716d954cdecae4f9f686b0317cba3
blueZhang <bluezhang521@163.com>
2017-03-21 17:52:56 +0800

    add .

--- app/src/main/java/com/bluezhang/baseappframwork/MainActivity.java
+++ app/src/main/java/com/bluezhang/baseappframwork/MainActivity.java
@@ -12,6 +12,7 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
+
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
